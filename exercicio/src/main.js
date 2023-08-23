41c32d6befcb56898e0faa5e370d61b0ee5fcad9
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-23 12:52:36 -0300

    update

--- exercicio/src/main.js
+++ exercicio/src/main.js
@@ -1,6 +1,19 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi'
+  }
+})
+
+createApp(App).use(vuetify).mount('#app')
